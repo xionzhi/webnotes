559915ebaff7af29341b0a9687c79a66fd27ed58
xionzhi <xionzhi@outlook.com>
2017-12-09 14:35:29 +0800

    mydemo_0503

--- 9_jquery/9.x/js/main.js
+++ 9_jquery/9.x/js/main.js
@@ -1,42 +1,81 @@
-/* 选中页面中所有的input[data-rule] */
-
-/* 解析每个input 中的验证规则 */
-
-/* 获取用户输入的值val() 进行规则验证 */
-
 $(function () {
-    // var validator = new Validator('Aasd123', {
-    //     max: 100,
-    //     min: 18,
-    //     maxlength: 18,
-    //     minlength: 5,
-    //     pattern: '^[a-zA-Z0-9]*$'
-    // });
+    'use strict';
 
-    // // 判断值 的大小
-    // var result1 = validator.validator_max();
-    // var result2 = validator.validator_min();
-    // console.log(result1 && result2);
+    /*选中页面中所有的input[data-rule]*/
+    var $inputs = $('[data-rule]')
+        , $form = $('#signup')
+        , inputs = [];
 
-    // // 判断值 的长度
-    // var result3 = validator.validator_maxlength();
-    // var result4 = validator.validator_minlength();
-    // console.log(result3&&result4);
+    $inputs.each(function (index, node) {
+        /*解析每一个input的验证规则*/
+        var tmp = new Input(node);
+        inputs.push(tmp);
+    })
 
-    // // 测试是否为 数字
-    // var result4 = validator.validator_numeric();
-    // console.log(result4)
+    $form.on('submit', function (e) {
+        e.preventDefault();
+        $inputs.trigger('blur');
 
-    // // 是否为空值 空格
-    // var result5 = validatoar.validator_required();
-    // console.log(result5)
+        inputs.forEach(function (item) {
+            var r = item.validator.is_valid();
+            if (!r) {
+                alert('invalid');
+                return;
+            }
+        });
 
-    // // 检测是否是否 只有字母数字
-    // var result6 =validator.validator_pattern()
-    // console.log(result6);
+        alert('valid');
+    })
 
-    // var test = new Input('#test');
-    // var valid = test.validator.is_valid();
-    // console.log('valid:', valid);
+    function signup() {
+        // $.post('/api/signup', {...})
+    }
 });
+
+// var r = item.validator.is_valid();
+// if (!r) {
+//     alert('invalid');
+//     return;
+// }
+
+// /* 选中页面中所有的input[data-rule] */
+
+// /* 解析每个input 中的验证规则 */
+
+// /* 获取用户输入的值val() 进行规则验证 */
+
+// $(function () {
+//     // var validator = new Validator('Aasd123', {
+//     //     max: 100,
+//     //     min: 18,
+//     //     maxlength: 18,
+//     //     minlength: 5,
+//     //     pattern: '^[a-zA-Z0-9]*$'
+//     // });
+
+//     // // 判断值 的大小
+//     // var result1 = validator.validator_max();
+//     // var result2 = validator.validator_min();
+//     // console.log(result1 && result2);
+
+//     // // 判断值 的长度
+//     // var result3 = validator.validator_maxlength();
+//     // var result4 = validator.validator_minlength();
+//     // console.log(result3&&result4);
+
+//     // // 测试是否为 数字
+//     // var result4 = validator.validator_numeric();
+//     // console.log(result4)
+
+//     // // 是否为空值 空格
+//     // var result5 = validatoar.validator_required();
+//     // console.log(result5)
+
+//     // // 检测是否是否 只有字母数字
+//     // var result6 =validator.validator_pattern()
+//     // console.log(result6);
+
+//     // var test = new Input('#test');
+//     // var valid = test.validator.is_valid();
+// });
 
--- 9_jquery/9.x/js/validator.js
+++ 9_jquery/9.x/js/validator.js
@@ -1,91 +1,180 @@
 $(function () {
     'use strict';
-
+  
     window.Validator = function (val, rule) {
-
-        /*{
-            max: 100,
-            min: 18
-        }*/
-        this.is_valid = function (new_val) {
-            var key;
-            val = new_val || val;
-
-            // 如果 值可以为空 直接返回
-            if (!rule.required && !val) {
-                return true
-            }
-
-            for (key in rule) {
-                // 防止重复检测
-                if (key === 'required') {
-                    continue;
-                }
-                // validator_max 拼接需要去验证的方法
-                // 调用 rule 中相对应的方法
-                var r = this['validator_' + key]();
-                // 如果有任何一条不通过 则 终止全部验证 
-                if (!r) return false;
-            }
-            //  验证全部通过
-            return true;
+  
+      /*
+      {
+        max: 10,
+        min: 2
+      }
+      */
+      this.is_valid = function (new_val) {
+        var key;
+        if (new_val !== undefined)
+          val = new_val;
+  
+        /*如果不是必填项且用户未填写任何内容则直接判定为合法*/
+        if (!rule.required && !val)
+          return true;
+  
+        for (key in rule) {
+          /*防止重复检查*/
+          if (key === 'required')
+            continue;
+  
+          /*调用rule中相对应的方法*/
+          var r = this['validate_' + key]();
+          if (!r) return false;
         }
-
-        // 验证  最大 值
-        this.validator_max = function () {
-            val = pre_max_min(val);
-            return val <= rule.max;
+  
+        return true;
+      }
+  
+      this.validate_max = function () {
+        pre_max_min();
+        return val <= rule.max;
+      }
+  
+      this.validate_min = function () {
+        pre_max_min();
+        return val >= rule.min;
+      }
+  
+      this.validate_maxlength = function () {
+        pre_length();
+        return val.length <= rule.maxlength;
+      }
+  
+      this.validate_minlength = function () {
+        pre_length();
+        return val.length >= rule.minlength;
+      }
+  
+      this.validate_numeric = function () {
+        return $.isNumeric(val);
+      }
+  
+      this.validate_required = function () {
+        var real = $.trim(val);
+        if (!real && real !== 0) {
+          return false;
         }
-
-        // 验证  最小 值
-        this.validator_min = function () {
-            val = pre_max_min(val);
-            return val >= rule.min;
-        }
-
-        // this.validator_max 或 this.validator_min 的前置工作
-        function pre_max_min(val) {
-            val = parseFloat(val);
-            return val;
-        }
-
-        // 验证 最大 长度
-        this.validator_maxlength = function () {
-            val = pre_length(val);
-            return val.length <= rule.maxlength;
-        }
-
-        // 验证 最小 长度
-        this.validator_minlength = function () {
-            val = pre_length(val);
-            return val.length >= rule.minlength;
-        }
-
-        // this.validator_maxlength 或 this.validator_minlength 的前置工作
-        function pre_length(val) {
-            val = val.toString();
-            return val;
-        }
-
-        // 验证 是否为 数字
-        this.validator_numeric = function () {
-            return $.isNumeric(val);
-        }
-
-        // 验证是否 为空值
-        this.validator_required = function () {
-            var real = $.trim(val) // 去除两端空格
-            if (!real && real !== 0) {
-                return false;
-            }
-
-            return true;
-        }
-
-        // 验证 只能有字母或者数字
-        this.validator_pattern = function () {
-            var reg = RegExp(rule.pattern);
-            return reg.test(val);
-        }
+        return true;
+      }
+  
+      this.validate_pattern = function () {
+        var reg = new RegExp(rule.pattern);
+        return reg.test(val);
+      }
+  
+      /* 用于完成this.validate_max 或
+        this.validate_min的前置工作
+      * */
+      function pre_max_min() {
+        val = parseFloat(val);
+      }
+  
+      /* 用于完成this.validate_maxlength或
+        this.validate_minlength的前置工作
+      * */
+      function pre_length() {
+        val = val.toString();
+      }
     }
-});
+  })
+
+  // $(function () {
+//     'use strict';
+
+//     window.Validator = function (val, rule) {
+
+//         /*{
+//             max: 100,
+//             min: 18
+//         }*/
+//         this.is_valid = function (new_val) {
+//             var key;
+//             val = new_val || val;
+
+//             // 如果 值可以为空 直接返回
+//             if (!rule.required && !val) {
+//                 return true
+//             }
+
+//             for (key in rule) {
+//                 // 防止重复检测
+//                 if (key === 'required') {
+//                     continue;
+//                 }
+//                 // validator_max 拼接需要去验证的方法
+//                 // 调用 rule 中相对应的方法
+//                 var r = this['validator_' + key]();
+//                 // 如果有任何一条不通过 则 终止全部验证 
+//                 if (!r) return false;
+//             }
+//             //  验证全部通过
+//             return true;
+//         }
+
+//         // 验证  最大 值
+//         this.validator_max = function () {
+//             val = pre_max_min(val);
+//             return val <= rule.max;
+//         }
+
+//         // 验证  最小 值
+//         this.validator_min = function () {
+//             val = pre_max_min(val);
+//             return val >= rule.min;
+//         }
+
+//         // this.validator_max 或 this.validator_min 的前置工作
+//         function pre_max_min(val) {
+//             val = parseFloat(val);
+//             return val;
+//         }
+
+//         // 验证 最大 长度
+//         this.validator_maxlength = function () {
+//             val = pre_length(val);
+//             return val.length <= rule.maxlength;
+//         }
+
+//         // 验证 最小 长度
+//         this.validator_minlength = function () {
+//             val = pre_length(val);
+//             return val.length >= rule.minlength;
+//         }
+
+//         // this.validator_maxlength 或 this.validator_minlength 的前置工作
+//         function pre_length(val) {
+//             val = val.toString();
+//             return val;
+//         }
+
+//         // 验证 是否为 数字
+//         this.validator_numeric = function () {
+//             return $.isNumeric(val);
+//         }
+
+//         // 验证是否 为空值
+//         this.validator_required = function () {
+//             var real = $.trim(val) // 去除两端空格
+//             if (!real && real !== 0) {
+//                 return false;
+//             }
+
+//             return true;
+//         }
+
+//         // 验证 只能有字母或者数字
+//         this.validator_pattern = function () {
+//             var reg = RegExp(rule.pattern);
+//             return reg.test(val);
+//         }
+//     }
+// });
+
+
+  
